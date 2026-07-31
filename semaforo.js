5422eee5eb5c6a0bc8208b7d15dc48db1bf0d4b9
comunicacion-hub <comunicacion@redesconrostro.org>
2026-07-30 22:08:17 -0500

    Update semaforo.js

--- semaforo.js
+++ semaforo.js
@@ -16,10 +16,6 @@ RCR.modulos.semaforo = {
   mount: function (root) {
     root.innerHTML =
       '<div class="section-header">' +
-        '<div>' +
-          '<div class="section-title">Mis registros</div>' +
-          '<div class="section-sub">Cómo te has sentido mes a mes</div>' +
-        '</div>' +
         '<div class="count-badge" id="sem-count">0</div>' +
       '</div>' +
       '<div id="sem-list"></div>';
